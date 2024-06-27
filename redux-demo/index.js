81a0d7f8f2dc708edfcf953efd7eb026c5c50752
Afirestriker <agrawalashish.in@gmail.com>
2024-06-27 21:22:26 +0530

    feat: implement redux store
    Successfully implemented redux pattern with its core concept and 3 basic principle (store, action, reducer)

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,3 +1,6 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
 const CAKE_ORDERED = 'CAKE_ORDERED';
 
 // action creator function that return an action
@@ -28,3 +31,20 @@ const reducer = (state = initialState, action) => {
             return state
     }
 }
+
+// responsiblity 1: Hold the application state
+const store = createStore(reducer);
+
+// responsiblity 2: export the getState() function to access the state
+console.log("Initial State", store.getState());
+
+// responsibility 4: add listner
+const unsubscribe = store.subscribe(() => console.log("Updated state", store.getState()));
+
+// responsibility 3: expose dispatch to allow action to be perform on the state
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+// responsibility 5: unsubscribe to listener
+unsubscribe();
