18f9d53505ae5fe2ca4de4b50017cba37361e763
Afirestriker <agrawalashish.in@gmail.com>
2024-06-27 21:20:20 +0530

    feat: implement redux reducer function

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -8,3 +8,23 @@ function orderCake() {
         quantity: 1
     }
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+    noOfCakes: 10
+}
+
+// state = initialState,
+// so when the application start's, the initial state of the application is pass in as a argument to the reducer function
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                noOfCakes: state.noOfCakes - 1
+            }
+        default:
+            return state
+    }
+}
